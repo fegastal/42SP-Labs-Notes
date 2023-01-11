a6492eee61cebf4e809f6dd5b74fa0aa87d9834f
Fernanda Gastal <fernanda.gastal.figueiredo@usp.br>
2023-01-11 13:14:49 -0300

    Create 'decoder' files

---
+++ srcs/decoder/main.c
@@ -0,0 +1,17 @@
+#include "../../includes/decoder.h"
+
+int	main()
+{
+	int i, vet[250];
+
+	for(i = 0; i < 250; i++)
+		vet[i] = 0;
+	
+	vet['V']++;
+	vet['a']++;
+	vet['m']++;
+	vet['o']++;
+	vet['s']++;
+
+	return 0;
+}
