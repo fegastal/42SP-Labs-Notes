19d21b3a2897988620b091b569f94c8395f5ad9d
Fernanda Gastal <fernanda.gastal.figueiredo@usp.br>
2023-01-11 16:17:30 -0300

    Frequency Table code

--- srcs/decoder/main.c
+++ srcs/decoder/main.c
@@ -1,17 +1,48 @@
 #include "../../includes/decoder.h"
 
+//-------------- parte 1: tabela de frequência --------------
+void	inicializa_tabela_com_zero(unsigned int tab[]) 
+{
+	int i;
+
+	for(i = 0; i < TAM; i++)
+		tab[i] = 0;
+}
+
+void	preenche_tab_frequencia(unsigned char texto[], unsigned int tab[])
+{
+	int i = 0;
+
+	while(texto[i] != '\0')
+	{
+		tab[texto[i]]++;
+		i++;
+	}
+}
+
+void imprime_tab_frequencia(unsigned int tab[])
+{
+	int i;
+
+	printf("\tTABELA DE FREQUENCIA\n");
+	for(i = 0; i < TAM; i++)
+	{
+		if(tab[i] > 0)
+			printf("\t%d = %d = %c\n", i, tab[i], i);
+	}
+}
+
 int	main()
 {
-	int i, vet[250];
+	unsigned char	texto[] = "Vamos aprender a programar";
+	unsigned int	tabela_frequencia[TAM];
 
-	for(i = 0; i < 250; i++)
-		vet[i] = 0;
-	
-	vet['V']++;
-	vet['a']++;
-	vet['m']++;
-	vet['o']++;
-	vet['s']++;
+	setlocale(LC_ALL, "Portuguese");
+
+	//-------------- parte 1: tabela de frequência --------------
+	inicializa_tabela_com_zero(tabela_frequencia);
+	preenche_tab_frequencia(texto, tabela_frequencia);
+	imprime_tab_frequencia(tabela_frequencia);
 
 	return 0;
 }
