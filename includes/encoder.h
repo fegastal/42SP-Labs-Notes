848b3c6a5fbc931ec91d9e27303d21963420ee0a
Fernanda Gastal <fernanda.gastal.figueiredo@usp.br>
2023-01-11 16:16:49 -0300

    Library and macro inclusion

--- includes/encoder.h
+++ includes/encoder.h
@@ -5,13 +5,16 @@
 
 // ======================== MACROS
 
+# define TAM 256
 
 // ======================== INCLUDES
 
 # include <unistd.h> // standard unix functions, like getpid(), write()
 # include <stdlib.h> // exit()
 # include <sys/types.h> //various type definitions, like pid_t
-#include <stdio.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <locale.h>
 
 // ======================== UTILS PROTOTYPES
 
