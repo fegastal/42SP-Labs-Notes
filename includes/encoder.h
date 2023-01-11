4e83cd3fcab71a096d004f24e95d2f7424a0148a
Fernanda Gastal <fernanda.gastal.figueiredo@usp.br>
2023-01-11 13:15:12 -0300

    Create 'encoder' files

---
+++ includes/encoder.h
@@ -0,0 +1,19 @@
+// ======================== DEFINITIONS
+
+#ifndef ENCODER_H
+# define ENCODER_H
+
+// ======================== MACROS
+
+
+// ======================== INCLUDES
+
+# include <unistd.h> // standard unix functions, like getpid(), write()
+# include <stdlib.h> // exit()
+# include <sys/types.h> //various type definitions, like pid_t
+#include <stdio.h>
+
+// ======================== UTILS PROTOTYPES
+
+
+#endif
