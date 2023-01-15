2915b8907a0222977aa64e3611bf36eeaf9ee456
Fernanda Gastal <fernanda.gastal.figueiredo@usp.br>
2023-01-15 12:36:31 -0300

    Update main.c

---
+++ sources/encoder/main.c
@@ -0,0 +1,567 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgastal- <fgastal-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 05:13:23 by fgastal-          #+#    #+#             */
+/*   Updated: 2022/11/13 05:22:06 by fgastal-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "encoder.h"
+
+//------------------------------- Utils Functions -------------------------------
+
+size_t	ft_strlen(const unsigned char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+//--------------------------- Part 1: Frequency Table ---------------------------
+
+void	initialize_table_with_zero(unsigned int tab[]) 
+{
+	int i;
+
+	for(i = 0; i < SIZE; i++)
+		tab[i] = 0;
+}
+
+void	fill_tab_frequency(unsigned char text[], unsigned int tab[])
+{
+	int i = 0;
+
+	while(text[i] != '\0')
+	{
+		tab[text[i]]++;
+		i++;
+	}
+}
+
+void print_tab_frequency(unsigned int tab[])
+{
+	int i;
+
+	printf("\tFREQUENCY TABLE:\n");
+	for(i = 0; i < SIZE; i++)
+	{
+		if(tab[i] > 0)
+			printf("\t%d = %u = %c\n", i, tab[i], i);
+	}
+}
+
+//------------------------- Part 2: Sorted Linked List  -------------------------
+
+void create_list(List *list)
+{
+	list->start = NULL;
+	list->size = 0;
+}
+
+void insert_ordered(List *list, Node *node)
+{
+	Node *aux;
+	// a list está vazia?
+	if(list->start == NULL)
+	{
+		// alterando o start da list
+		list->start = node;
+		//list->size++;
+
+	}
+	// pode ser que a list não esteja vazia,
+	// mas vamos inserir de forma ordenada
+	// tem freq menor que o start da list
+	else if(node->frequency < list->start->frequency)
+	{
+		node->next = list->start;
+		list->start = node;
+		//list->size++;
+	}
+	else
+	{
+		// criei um ponteiro auxiliar para o start da list
+		aux = list->start;
+		// o novo nó vai ficar antes e apos o aux
+		while(aux->next && aux->next->frequency <= node->frequency)
+			aux = aux->next;
+		// conectar o novo nó na nossa list
+		node->next = aux->next;
+		aux->next = node;
+		//list->size++;
+	}
+	list->size++;
+}
+
+void fill_list(unsigned int tab[], List *list)
+{
+	// precisamos percorrer essa tabela de 0 a 255
+	// olhar qual posição é maior que 0 (frequência 1)
+	int i;
+
+	Node *new;
+	for(i = 0; i < SIZE; i++)
+	{
+		if(tab[i] > 0)
+		{
+			new = malloc(sizeof(Node));
+			if(new){
+				new->character = i;
+				new->frequency = tab[i];
+				new->right = NULL;
+				new->left = NULL;
+				new->next = NULL;
+
+				insert_ordered(list, new);
+			}
+			else
+			{
+				printf("\tError when allocating memory in fill_list");
+				break;
+			}
+		}
+	}
+}
+
+void print_list(List *list)
+{
+	Node *aux = list->start;
+
+	printf("\n\tSorted Linked List: Size: %d\n", list->size);
+	while(aux)
+	{
+		printf("\tCharacter: %c Frequency: %d\n", aux->character, aux->frequency);
+		aux = aux->next;
+	}
+}
+
+//---------------------- Part 3: Building Huffman's Tree  ----------------------
+
+Node* remove_at_start(List *list)
+{
+	Node *aux = NULL;
+
+	if(list->start)
+	{
+		aux = list->start;
+		list->start = aux->next;
+		aux->next = NULL;
+		list->size--;
+	}
+
+	return aux;
+}
+
+Node* assemble_tree(List *list)
+{
+	Node *first;
+	Node *second;
+	Node *new;
+
+	while(list->size > 1)
+	{
+		first = remove_at_start(list);
+		second = remove_at_start(list);
+		new = malloc(sizeof(Node));
+
+		if(new)
+		{
+			new->character = '+';
+			new->frequency = first->frequency + second->frequency;
+			new->left = first;
+			new->right = second;
+			new->next = NULL;
+
+			insert_ordered(list, new);
+		}
+		else
+		{
+			printf("\n\tError when allocating memory in assemble_tree!");
+			break; // tbm poderia ser return null em vez de break
+		}
+	}
+	return list->start;
+}
+
+void print_tree(Node *root, int size)
+{
+	if(root->left == NULL && root->right == NULL)
+		printf("\tLeaf: %c\t Height: %d\n", root->character, size);
+	else
+	{
+		print_tree(root->left, size + 1);
+		print_tree(root->right, size + 1);
+	}
+}
+
+void destroy_tree(Node *root)
+{
+	if(root)
+	{
+		destroy_tree(root->left);
+		destroy_tree(root->right);
+		free(root);
+	}
+}
+
+//---------------------- Part 4: Building the Dictionary  ----------------------
+
+int height_tree(Node *root)
+{
+	int left;
+	int right;
+
+	// se a raiz for nula, chegamos a uma folha
+	if(root == NULL)
+		return -1;
+	else
+	{
+		left = height_tree(root->left) + 1;
+		right = height_tree(root->right) + 1;
+		if(left > right)
+			return left;
+		else
+			return right;
+	}
+}
+
+char** allocate_dictionary(int columns)
+{
+	char **dictionary;
+	int i;
+
+	dictionary = malloc(sizeof(char*) * SIZE);
+
+	for(i = 0; i < SIZE; i++)
+		// cada posição desse vetor irá apontar para outro vetor
+		// ou seja, uma matriz
+		// calloc aloca em uma região de memória 
+		// e retorna o endereço dessa memória (limpa o que tem naquela
+		// região de memória -> precisamos fazer isso porque
+		// vamos ter que concatenar strings
+		dictionary[i] = calloc(columns, sizeof(char));
+	return dictionary;
+}
+
+// aqui vou produzir o meu dicionário
+// recebe meu dicionario / tabela como primeiro parametro
+// procedimento recursivo: conforme eu for caminhando na minha
+// arvore, p/ esquerda ou direita, eu vou concatenando em 0 ou 1
+// ou seja, o caminho é o código do caractere até chegar em uma folha
+
+void generate_dictionary(char **dictionary, Node *root, char *path, int columns)
+{
+	char left[columns];
+	char right[columns];
+
+	if(root->left == NULL && root->right == NULL)
+	{
+		// destino, caminho
+		strcpy(dictionary[root->character], path);
+	}
+	else
+	{
+		// precisamos caminhar pela arvore inteira
+		strcpy(left, path);
+		strcpy(right, path);
+
+		// aqui estou concatenando o 0 e 1
+		strcat(left,"0");
+		strcat(right, "1");
+
+		generate_dictionary(dictionary, root->left,left, columns);
+		generate_dictionary(dictionary, root->right, right, columns);
+	}
+}
+
+void print_dictionary(char **dictionary)
+{
+	int i;
+
+	for(i = 0; i < SIZE; i++)
+	{
+		if(ft_strlen((unsigned char *) dictionary[i]) > 0)
+			printf("\t%3d: %s\n", i, dictionary[i]);
+	}
+}
+
+//----------------------------- Part 5: Encode  -----------------------------
+
+/*
+* text[i] vai virar um número representado em ASCII
+*/
+int calculates_size_string(char ** dictionary, unsigned char *text)
+{
+	int i;
+	int size;
+
+	i = 0;
+	size = 0;
+	while(text[i] != '\0')
+	{
+		size = size + ft_strlen((unsigned char *) dictionary[text[i]]);
+		i++;
+	}
+	return size + 1;
+}
+
+/*
+* Se, em um texto, eu tiver 35 caracteres, 
+* eu preciso percorrer o meu dicionário procurando qual o size de cada um desses caracteres
+* Lembrando-se que acrescenta-se + 1 por causa do '\0' no final de string
+*/
+char* encode(char ** dictionary, unsigned char *text)
+{
+	int i;
+	int size;
+
+	i = 0;
+	size = calculates_size_string(dictionary, text);
+	// calloc recebe:
+	// a quant de caracteres de um vetor
+	// o size de um caractere
+	char *code = calloc(size, sizeof(char));
+	while(text[i] != '\0')
+	{
+		strcat(code, dictionary[text[i]]);
+		i++;
+	}
+	return code;
+}
+
+//------------------------------ Part 6: Decode  ----------------------------
+
+char* decode(unsigned char text[], Node *root)
+{
+	int i = 0;
+
+	Node *aux = root;
+	char temp[2];
+	char *decoded = calloc(ft_strlen(text), sizeof(char));
+
+	while(text[i] != '\0')
+	{
+		if(text[i] == '0')
+			aux = aux->left;
+		else
+			aux = aux->right;
+		if(aux->left == NULL && aux->right == NULL)
+		{
+			temp[0] = aux->character;
+			temp[1] = '\0';
+			strcat(decoded, temp);
+			aux = root;
+		}
+		i++;
+	}
+	return decoded;
+}
+
+//---------------------------- Part 7: Compress  ----------------------------
+
+void compress(unsigned char str[])
+{
+	unsigned char mask;
+	unsigned char byte;
+
+	FILE *file = fopen("compressed.wg", "wb"); //write binary
+	int i = 0;
+	int j = 7;
+	byte = 0;
+	if(file)
+	{
+		while(str[i] != '\0')
+		{
+			mask = 1;
+			if(str[i] == '1')
+			{
+				mask = mask << j;
+				byte = byte | mask;
+			}
+			j--; // j precisa decrementar porque começou com 7
+			if(j < 0) // tem um byte formado
+			{
+				fwrite(&byte, sizeof(unsigned char), 1, file);
+				byte = 0; // toda vez que escrevemos 1 byte em um arquivo ele volta para 7
+				j = 7;
+			}
+			i++;
+		}
+		// existe 1 bit pela metade ou foi finalizado certinho?
+		if(j != 7) // existe um byte em formação (?)
+			fwrite(&byte, sizeof(unsigned char), 1, file);
+		fclose(file);
+	}
+	else
+		printf("\nError when opening / creating file in compress\n");
+}
+
+//---------------------------- Part 8: Decompress  ----------------------------
+
+unsigned int is_bit_one(unsigned char byte, int i)
+{
+	unsigned char mask;
+
+	mask = (1 << i); // 00000001
+	return byte & mask; // 10000000
+}
+
+/*
+	O retorno é zero ou maior que zero, se o bit mais à esquerda
+	for 1, o retorno será 128 que é 10000000 em binário
+*/
+void decompress(Node *root)
+{
+	FILE *file = fopen("compressed.wg", "rb"); //read binary
+	Node *aux = root;
+	unsigned char byte; // 10111001
+	int i;
+
+	if(file)
+	{
+		while(fread(&byte, sizeof(unsigned char), 1, file))
+		{
+			for(i= 7; i >= 0; i--)
+			{
+				if(is_bit_one(byte, i))
+					// então quando for 1, precisamos caminhar para a direita
+					// portanto, precisamos do nosso ponteiro para caminhar na arvore
+					aux = aux->right;
+				else
+					aux = aux->left;
+				if(aux->left == NULL && aux->right == NULL)
+				{
+					// aqui, significa que estamos em uma folha da nossa árvore
+					printf("%c", aux->character);
+					aux = root;
+				}
+			}
+		}
+		fclose(file);
+	}
+	else
+		printf("\nError when opening file in unzip\n");
+}
+
+int	find_size()
+{
+	FILE *file = fopen("./tests/teste.txt", "r");
+	int size = 0;
+
+	if(file)
+	{
+		while(fgetc(file) != -1)
+			size++;
+		fclose(file);
+	}
+	else
+		printf("\nError when opening file in discover_size\n");
+	return size;
+}
+
+void read_text(unsigned char *text)
+{
+	FILE *file = fopen("./tests/teste.txt", "r");
+	char letter;
+	int i = 0;
+
+	if(file)
+	{
+		while(!feof(file))
+		{
+			letter = fgetc(file);
+			if(letter != -1)
+			{
+				text[i] = letter;
+				i++;
+			}
+		}
+		fclose(file);
+	}
+	else
+		printf("\nError when opening file in read_text\n");
+}
+
+//---------------------------- Part 9: Main  ----------------------------
+
+int	main()
+{
+	//unsigned char	text[] = "Insert some text here";
+	unsigned char	*text;
+	unsigned int	table_frequency[SIZE];
+	List list;
+	Node *tree;
+	int columns;
+	int size;
+	char **dictionary;
+	unsigned char *encoded;
+	unsigned char *decoded;
+
+	setlocale(LC_ALL, "utf8");
+
+	size = find_size();
+	printf("\nQUANTITY: %d\n", size);
+
+	text = calloc(size + 2, sizeof(unsigned char));
+	read_text(text);
+	//printf("\nTEXT:\n%s\n", text);
+
+//--------------------------- Part 1: Frequency Table ---------------------------
+
+	initialize_table_with_zero(table_frequency);
+	fill_tab_frequency(text, table_frequency);
+	//print_tab_frequency(table_frequency);
+
+//------------------------- Part 2: Sorted Linked List  -------------------------
+
+	create_list(&list);
+	fill_list(table_frequency, &list);
+	//print_list(&list);
+
+//---------------------- Part 3: Building Huffman's Tree  ----------------------
+
+	tree = assemble_tree(&list);
+	//printf("\n\tHUFFMAN'S TREE\n");
+	//print_tree(tree, 0);
+
+//---------------------- Part 4: Building the Dictionary  ----------------------
+
+	columns = height_tree(tree) + 1;
+	dictionary = allocate_dictionary(columns);
+	//printf("\n\tDICTIONARY:\n");
+	generate_dictionary(dictionary, tree, "", columns);
+	//print_dictionary(dictionary);
+
+//----------------------------- Part 5: Encode  -----------------------------
+
+	encoded = (unsigned char *) encode(dictionary, text);
+	//printf("\n\tCODED TEXT: %s\n", encoded);
+
+//------------------------------ Part 6: Decode  ----------------------------
+
+	decoded = (unsigned char *) decode(encoded, tree);
+	//printf("\n\tENCODED TEXT: %s\n", encoded);
+
+//---------------------------- Part 7: Compress  ----------------------------
+
+	compress(encoded);
+
+//---------------------------- Part 8: Decompress  ----------------------------
+
+	printf("\nUNZIPPED FILE!\n");
+	decompress(tree);
+	printf("\n\n");
+
+	free(text);
+	free(encoded);
+	free(decoded);
+
+	return 0;
+}
