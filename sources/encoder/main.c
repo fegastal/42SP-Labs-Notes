b3aa1f74445e3fe679418f906139bd67590cbc0a
Fernanda Gastal <fernanda.gastal.figueiredo@usp.br>
2023-01-15 15:09:30 -0300

    Update main.c

--- sources/encoder/main.c
+++ sources/encoder/main.c
@@ -10,10 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "encoder.h"
+/**
+* main.c
+* A simple file compressor using Huffman trees.
+* @ver 0.4
+* @autores: Fernanda Gastal
+*/
+
+#include "decoder.h"
 
 //------------------------------- Utils Functions -------------------------------
 
+/**
+* The function ft_strlen was created to use strlen as a basis,
+* but getting a const unsigned char *str instead of a const char.
+* It is intended to return the size of a string.
+*/
 size_t	ft_strlen(const unsigned char *str)
 {
 	size_t	i;
@@ -26,6 +38,9 @@ size_t	ft_strlen(const unsigned char *str)
 
 //--------------------------- Part 1: Frequency Table ---------------------------
 
+/**
+* Function that starts the table with the value zero.
+*/
 void	initialize_table_with_zero(unsigned int tab[]) 
 {
 	int i;
@@ -34,6 +49,9 @@ void	initialize_table_with_zero(unsigned int tab[])
 		tab[i] = 0;
 }
 
+/**
+* Function that fills the frequency table from the received text.
+*/
 void	fill_tab_frequency(unsigned char text[], unsigned int tab[])
 {
 	int i = 0;
@@ -45,6 +63,9 @@ void	fill_tab_frequency(unsigned char text[], unsigned int tab[])
 	}
 }
 
+/**
+* Function that prints the frequency table from the received text.
+*/
 void print_tab_frequency(unsigned int tab[])
 {
 	int i;
@@ -59,51 +80,47 @@ void print_tab_frequency(unsigned int tab[])
 
 //------------------------- Part 2: Sorted Linked List  -------------------------
 
+/**
+* Function that creates an ordered list.
+*/
 void create_list(List *list)
 {
 	list->start = NULL;
 	list->size = 0;
 }
 
+/**
+* Function that inserts a node in an ordered linked list.
+*/
 void insert_ordered(List *list, Node *node)
 {
 	Node *aux;
-	// a list está vazia?
+
 	if(list->start == NULL)
 	{
-		// alterando o start da list
 		list->start = node;
-		//list->size++;
-
 	}
-	// pode ser que a list não esteja vazia,
-	// mas vamos inserir de forma ordenada
-	// tem freq menor que o start da list
 	else if(node->frequency < list->start->frequency)
 	{
 		node->next = list->start;
 		list->start = node;
-		//list->size++;
 	}
 	else
 	{
-		// criei um ponteiro auxiliar para o start da list
 		aux = list->start;
-		// o novo nó vai ficar antes e apos o aux
 		while(aux->next && aux->next->frequency <= node->frequency)
 			aux = aux->next;
-		// conectar o novo nó na nossa list
 		node->next = aux->next;
 		aux->next = node;
-		//list->size++;
 	}
 	list->size++;
 }
 
+/**
+* Function that fills the list with nodes in an ordered (ascending) manner.
+*/
 void fill_list(unsigned int tab[], List *list)
 {
-	// precisamos percorrer essa tabela de 0 a 255
-	// olhar qual posição é maior que 0 (frequência 1)
 	int i;
 
 	Node *new;
@@ -130,6 +147,9 @@ void fill_list(unsigned int tab[], List *list)
 	}
 }
 
+/**
+* Function that prints the list on the terminal.
+*/
 void print_list(List *list)
 {
 	Node *aux = list->start;
@@ -144,6 +164,9 @@ void print_list(List *list)
 
 //---------------------- Part 3: Building Huffman's Tree  ----------------------
 
+/**
+* Function that removes the first two lowest frequency nodes.
+*/
 Node* remove_at_start(List *list)
 {
 	Node *aux = NULL;
@@ -159,6 +182,29 @@ Node* remove_at_start(List *list)
 	return aux;
 }
 
+/*
+Algoritmo para construir a árvore de huffman, inspirado no seguinte pseudocódigo:
+<http://www.cs.gettysburg.edu/~ilinkin/courses/Spring-2014/cs216/assignments/a8.html>
+
+procedure BUILD-TREE(frequencies):
+    pq ← make empty priority queue
+    for each (symbol, key) in frequencies:
+        h ← make a leaf node for the (symbol, key) pair
+        INSERT(pq, h)
+
+    n ← size[pq]
+    for i = 1 to n-1:
+        h1 ← POP-MIN(pq)
+        h2 ← POP-MIN(pq)
+        h3 ← MAKE-NODE(h1, h2)
+        INSERT(pq, h3)
+
+    return POP-MIN(pq)
+*/
+
+/** Função que constrói a árvore de huffman
+* @param: a fila de prioridade.
+*/
 Node* assemble_tree(List *list)
 {
 	Node *first;
@@ -170,7 +216,6 @@ Node* assemble_tree(List *list)
 		first = remove_at_start(list);
 		second = remove_at_start(list);
 		new = malloc(sizeof(Node));
-
 		if(new)
 		{
 			new->character = '+';
@@ -184,12 +229,15 @@ Node* assemble_tree(List *list)
 		else
 		{
 			printf("\n\tError when allocating memory in assemble_tree!");
-			break; // tbm poderia ser return null em vez de break
+			break;
 		}
 	}
 	return list->start;
 }
 
+/**
+* Function that prints the Huffman's tree.
+*/
 void print_tree(Node *root, int size)
 {
 	if(root->left == NULL && root->right == NULL)
@@ -201,6 +249,9 @@ void print_tree(Node *root, int size)
 	}
 }
 
+/**
+* Function to destroy the nodes of the tree and release the stored memory.
+*/
 void destroy_tree(Node *root)
 {
 	if(root)
@@ -213,12 +264,15 @@ void destroy_tree(Node *root)
 
 //---------------------- Part 4: Building the Dictionary  ----------------------
 
+/**
+* Function to check the height of the binary tree. 
+* Note that + 1 is for the null character '\0'.
+*/
 int height_tree(Node *root)
 {
 	int left;
 	int right;
 
-	// se a raiz for nula, chegamos a uma folha
 	if(root == NULL)
 		return -1;
 	else
@@ -232,6 +286,13 @@ int height_tree(Node *root)
 	}
 }
 
+/**
+* Function that allocates memory for the dictionary, 
+* receiving the value of the number of columns (which is the 
+* height of the Huffman tree + 1)
+* Important to note that the number of rows, in turn, is the 
+* size of the ASCII table (macro SIZE).
+*/
 char** allocate_dictionary(int columns)
 {
 	char **dictionary;
@@ -240,22 +301,18 @@ char** allocate_dictionary(int columns)
 	dictionary = malloc(sizeof(char*) * SIZE);
 
 	for(i = 0; i < SIZE; i++)
-		// cada posição desse vetor irá apontar para outro vetor
-		// ou seja, uma matriz
-		// calloc aloca em uma região de memória 
-		// e retorna o endereço dessa memória (limpa o que tem naquela
-		// região de memória -> precisamos fazer isso porque
-		// vamos ter que concatenar strings
 		dictionary[i] = calloc(columns, sizeof(char));
 	return dictionary;
 }
 
-// aqui vou produzir o meu dicionário
-// recebe meu dicionario / tabela como primeiro parametro
-// procedimento recursivo: conforme eu for caminhando na minha
-// arvore, p/ esquerda ou direita, eu vou concatenando em 0 ou 1
-// ou seja, o caminho é o código do caractere até chegar em uma folha
-
+/**
+* In this function I will produce (generate) my dictionary. I get some parameters
+* like the dictionary, the root node, the path and the columns.
+* Note that this is a recursive procedure: as I go 
+* in my tree to the left(0) or to the right(1), I concatenate (with strcat) to 0 or 1,
+* and the path is precisely the code of the character generated until it reaches a leaf 
+* (that is, a non-intermediate node, with no children).
+*/
 void generate_dictionary(char **dictionary, Node *root, char *path, int columns)
 {
 	char left[columns];
@@ -263,16 +320,13 @@ void generate_dictionary(char **dictionary, Node *root, char *path, int columns)
 
 	if(root->left == NULL && root->right == NULL)
 	{
-		// destino, caminho
 		strcpy(dictionary[root->character], path);
 	}
 	else
 	{
-		// precisamos caminhar pela arvore inteira
 		strcpy(left, path);
 		strcpy(right, path);
 
-		// aqui estou concatenando o 0 e 1
 		strcat(left,"0");
 		strcat(right, "1");
 
@@ -281,6 +335,9 @@ void generate_dictionary(char **dictionary, Node *root, char *path, int columns)
 	}
 }
 
+/**
+* Function for printing the dictionary.
+*/
 void print_dictionary(char **dictionary)
 {
 	int i;
@@ -295,7 +352,8 @@ void print_dictionary(char **dictionary)
 //----------------------------- Part 5: Encode  -----------------------------
 
 /*
-* text[i] vai virar um número representado em ASCII
+* Function that calculates the string size, given the dictionary and the text
+* as parameters. Note that text[i] will become a number represented in ASCII.
 */
 int calculates_size_string(char ** dictionary, unsigned char *text)
 {
@@ -313,9 +371,10 @@ int calculates_size_string(char ** dictionary, unsigned char *text)
 }
 
 /*
-* Se, em um texto, eu tiver 35 caracteres, 
-* eu preciso percorrer o meu dicionário procurando qual o size de cada um desses caracteres
-* Lembrando-se que acrescenta-se + 1 por causa do '\0' no final de string
+* Function responsible for doing the encoding of our text.
+* Receives as parameter the dictionary and the strings.
+* Note that calloc takes the number of characters from an array
+* and the size of a character.
 */
 char* encode(char ** dictionary, unsigned char *text)
 {
@@ -338,6 +397,10 @@ char* encode(char ** dictionary, unsigned char *text)
 
 //------------------------------ Part 6: Decode  ----------------------------
 
+/*
+* Function responsible for the decoding - the process 
+* inverse to the codification - of a text. 
+*/
 char* decode(unsigned char text[], Node *root)
 {
 	int i = 0;
@@ -366,6 +429,12 @@ char* decode(unsigned char text[], Node *root)
 
 //---------------------------- Part 7: Compress  ----------------------------
 
+/*
+* This function is responsible for compressing our file, receiving strings 
+* as argument. Here we can see that it was necessary to manipulate 
+* bits with Shift Left.
+* Note: wb = write binary.
+*/
 void compress(unsigned char str[])
 {
 	unsigned char mask;
@@ -405,6 +474,11 @@ void compress(unsigned char str[])
 
 //---------------------------- Part 8: Decompress  ----------------------------
 
+/*
+* Function responsible for helping decompress a file, receiving as argument 
+* the bytes and an integer value i to be able to Shift left, 
+* and checking if the byte is equal to 1.
+*/
 unsigned int is_bit_one(unsigned char byte, int i)
 {
 	unsigned char mask;
@@ -414,8 +488,9 @@ unsigned int is_bit_one(unsigned char byte, int i)
 }
 
 /*
-	O retorno é zero ou maior que zero, se o bit mais à esquerda
-	for 1, o retorno será 128 que é 10000000 em binário
+* Function responsible for decompressing a file, 
+* taking as argument the root node of our binary tree. 
+* Note: "rb" = read binary.
 */
 void decompress(Node *root)
 {
@@ -450,6 +525,10 @@ void decompress(Node *root)
 		printf("\nError when opening file in unzip\n");
 }
 
+/*
+* Function responsible for returning the size of the 
+* .txt file in order to check the compression capacity.
+*/
 int	find_size()
 {
 	FILE *file = fopen("./tests/teste.txt", "r");
@@ -466,6 +545,10 @@ int	find_size()
 	return size;
 }
 
+/*
+* Function responsible for reading the text (.txt) 
+* passed as argument to the function.
+*/
 void read_text(unsigned char *text)
 {
 	FILE *file = fopen("./tests/teste.txt", "r");
@@ -491,6 +574,15 @@ void read_text(unsigned char *text)
 
 //---------------------------- Part 9: Main  ----------------------------
 
+/*
+* The main function of the program is very important to link all the 
+* previous functions and form the program. It is important to note that 
+* the "setlocale" was used in order to make accent / punctuation characters 
+* also readable from UTF-8.
+
+* From this we can see all the processes involved, as well as the encoding 
+* and decoding going on.
+*/
 int	main()
 {
 	//unsigned char	text[] = "Insert some text here";
@@ -511,43 +603,43 @@ int	main()
 
 	text = calloc(size + 2, sizeof(unsigned char));
 	read_text(text);
-	//printf("\nTEXT:\n%s\n", text);
+	printf("\nTEXT:\n%s\n", text);
 
 //--------------------------- Part 1: Frequency Table ---------------------------
 
 	initialize_table_with_zero(table_frequency);
 	fill_tab_frequency(text, table_frequency);
-	//print_tab_frequency(table_frequency);
+	print_tab_frequency(table_frequency);
 
 //------------------------- Part 2: Sorted Linked List  -------------------------
 
 	create_list(&list);
 	fill_list(table_frequency, &list);
-	//print_list(&list);
+	print_list(&list);
 
 //---------------------- Part 3: Building Huffman's Tree  ----------------------
 
 	tree = assemble_tree(&list);
-	//printf("\n\tHUFFMAN'S TREE\n");
-	//print_tree(tree, 0);
+	printf("\n\tHUFFMAN'S TREE\n");
+	print_tree(tree, 0);
 
 //---------------------- Part 4: Building the Dictionary  ----------------------
 
 	columns = height_tree(tree) + 1;
 	dictionary = allocate_dictionary(columns);
-	//printf("\n\tDICTIONARY:\n");
+	printf("\n\tDICTIONARY:\n");
 	generate_dictionary(dictionary, tree, "", columns);
-	//print_dictionary(dictionary);
+	print_dictionary(dictionary);
 
 //----------------------------- Part 5: Encode  -----------------------------
 
 	encoded = (unsigned char *) encode(dictionary, text);
-	//printf("\n\tCODED TEXT: %s\n", encoded);
+	printf("\n\tCODED TEXT: %s\n", encoded);
 
 //------------------------------ Part 6: Decode  ----------------------------
 
 	decoded = (unsigned char *) decode(encoded, tree);
-	//printf("\n\tENCODED TEXT: %s\n", encoded);
+	printf("\n\tENCODED TEXT: %s\n", encoded);
 
 //---------------------------- Part 7: Compress  ----------------------------
 
@@ -562,6 +654,7 @@ int	main()
 	free(text);
 	free(encoded);
 	free(decoded);
+	destroy_tree(tree);
 
 	return 0;
 }
